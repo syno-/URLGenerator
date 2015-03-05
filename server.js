1890cf4307e915d4f0161c60c81af2d899cb92cf
root <root@stun2.syno.jp>
2015-03-05 16:59:15 +0900

    multi team

--- server.js
+++ server.js
@@ -2,37 +2,39 @@
 var config = require('./config/default.json');
 var http = require('http');
 var qs = require('qs');
-
 var request = require('request');
 
+var uris = {
+"dsv2oGnrCq3TPfSeXmNH4A8J": "https://hooks.slack.com/services/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",
+"BANg76qKfv2PhOFI0a0KHlPb": "https://hooks.slack.com/services/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"
+};
 
 http.createServer(function (req, res) {
   var data = '';
-  req.on('data', function(chunk) { data += chunk; });
-  req.on('end', function() {
+  req.on('data', function (chunk) { data += chunk; });
+  req.on('end', function () {
     var params = qs.parse(data);
-    var options = {
-      uri: "https://hooks.slack.com/services/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"
-    };
+    var options = { "uri": uris[params.token] };
     options.form = JSON.stringify( {
-        username: "syno",
-        icon_emoji: ":ghost:",
-        channel: '#' + params.channel_name,
-        text: params.user_name + ' open a room: <http://syno.in/vchat.html?' +
+//        username: "syno",
+//        icon_emoji: ":ghost:",
+//        channel: (params.channel_name === "directmessage")? '@' : 
+//                 (params.channel_name === "privategroup") : "" : "#" + params.channel_name,
+        channel: params.channel_id,
+//        channel_id: params.channel_id,
+        text: '@' + params.user_name + ' open a room: <http://syno.in/vchat.html?' +
               params.text + '|' + params.text + '>'
     });
     request.post(options, function(error, response, body){
-      if (!error && response.statusCode == 200) {
-        console.log(body.name);
-      } else {
+      if ( response.statusCode != 200) {
         console.log('error: '+ response.statusCode + body);
       }
     });
 //    res.writeHead(200, {'Content-Type': 'application/json'});
 //    res.write('{ "text": "http://syno.in/vchat.html?' + qs.parse(data).text + '", "username": "vv" }');
     res.end();
-  }).on('error', function(e){
-    console.log(e.message);
+  }).on('error', function (error){
+    console.log(error.message);
   });
 }).listen(config.port);
 
